d8c9d311305c6c87d40dfd4a6ea2b3bd372399ac
montezakarim <kmonteza@hotmail.com>
2019-08-06 12:10:38 -0500

    firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-// Este es el punto de entrada de tu aplicacion
+// // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+const name = document.getElementById ('nombre');
+const checkIn = document.getElementById('registrar');
+checkIn.addEventListener('click',() => {
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('contrasena').value;
+    const rPassword=document.getElementById('confirmar-contrasenna').value; 
+ console.log(email);
+ Console.log(contrasena)
+    Console.log('dar click')
+});
+// firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+//   });email-password.html
