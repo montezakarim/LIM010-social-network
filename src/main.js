7fa25718539bf82c3b669023875459f7dc4b3b8c
melizasosa <s.m.meliza@gmail.com>
2019-08-06 22:02:43 -0500

    Autentificar y crear usuario

--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+/*import { myFunction } from './lib/index.js';
 
-myFunction();
+myFunction();*/
+(function(){
+    const firebaseConfig = {
+        apiKey: "AIzaSyAY3VOT2W4EKd4wxEfo759lF2sjMk6Hy4I",
+        authDomain: "sweet-diet-5c3e2.firebaseapp.com",
+        databaseURL: "https://sweet-diet-5c3e2.firebaseio.com",
+        projectId: "sweet-diet-5c3e2",
+        storageBucket: "",
+        messagingSenderId: "114566620755",
+        appId: "1:114566620755:web:0b0b489f58560e4e"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+      // Obtener elementos
+      const txtEmail=document.getElementById('txt-email');
+      const txtPassword=document.getElementById('txt-password');
+      const btnLogin=document.getElementById('btn-login');
+      const txtNameAdd=document.getElementById('txt-name-regist-add');
+      const txtEmailAdd=document.getElementById('txt-email-add');
+      const txtPasswordAdd=document.getElementById('txt-password-add');
+      const txtConfirPasswordAdd=document.getElementById('txt-confir-password-add');
+      const btnAdd=document.getElementById('btn-add');
+
+      //Añadir evento login
+      btnLogin.addEventListener('click',e =>{
+          const email = txtEmail.value;
+          const password = txtPassword.value;
+
+          const promise=firebase.auth().signInWithEmailAndPassword(email, password);
+          promise.catch(e => console.log(e.message));
+      });
+
+      //Añadir evento registrar
+      btnAdd.addEventListener('click',e =>{
+        // obtener email y password
+        const nameAdd = txtNameAdd.value;
+        const emailAdd = txtEmailAdd.value;
+        const passwordAdd = txtPasswordAdd.value;
+        const confirmPasswordAdd = txtConfirPasswordAdd.value;
+
+        const promise=firebase.auth().createUserWithEmailAndPassword(emailAdd, passwordAdd);
+        promise.catch(e => console.log(e.message));
+    });
+
+}());
