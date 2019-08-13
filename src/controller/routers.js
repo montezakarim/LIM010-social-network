4a8d7a150467bf7d462582e2b168e3daf46f4cfe
montezakarim <kmonteza@hotmail.com>
2019-08-13 13:00:58 -0500

    slintrc.js

--- src/controller/routers.js
+++ src/controller/routers.js
@@ -1,33 +1,32 @@
-import { components } from '../views/components.js'
+import { components } from '../views/components.js';
+
 export const changeView = (routers) => {
   const container = document.getElementById('container');
   container.innerHTML = '';
   switch (routers) {
-    case '#/':
-      {
-        container.appendChild(components.home())
+    case '#/': {
+      container.appendChild(components.home());
       }
       break;
     case '#/register':
       {
-        container.appendChild(components.register())
+        container.appendChild(components.register());
       }
       break;
     case '#/login':
       {
-        container.appendChild(components.login())
+        container.appendChild(components.login());
       }
       break;
     case '#/profile':
       {
-        container.appendChild(components.profile())
+        container.appendChild(components.profile());
       }
       break;
 
-    default: 
-    {
-      container.appendChild(components.home())
+    default: {
+      container.appendChild(components.login());
     }
       break;
   }
-}
+};
