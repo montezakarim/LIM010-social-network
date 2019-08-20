3346e49333dab943f2963a888be9917259ae8021
montezakarim <kmonteza@hotmail.com>
2019-08-20 13:02:19 -0500

    home

--- src/controller/post-controller.js
+++ src/controller/post-controller.js
@@ -1,21 +1,30 @@
-import { addPost, userCurrent } from '../firebase/controllerdata.js';
+import { addPost, userCurrent, deletePostFirebase } from '../firebase/controllerdata.js';
 
 export const functionPostAdd = (event) => {
   event.preventDefault();
   const newPost = document.getElementById('new-post').value;
   const user = userCurrent();
   const privacyUser = document.getElementById('privacy').value;
-    if (newPost === '' || newPost === ' ') {
-      alert('Ingresa un contenido');
-    } else {
-      addPost(newPost, user, privacyUser)
+  if (newPost === '' || newPost === ' ') {
+    alert('Ingresa un contenido');
+  } else {
+    addPost(newPost, user, privacyUser)
       .then(() => {
         alert('Post Ingresado');
         console.log(newPost);
       })
-      .catch(() => {
+      .catch((error) => {
         console.error('Error adding document: ', error);
-      }); 
-    } 
+      });
+  }
 };
 
+export const deletePost = (id) => {
+  deletePostFirebase(id)
+    .then(() => {
+      deletePostFirebase(id);
+      // console.log('Document written with ID: ', docRef.id);
+    }).catch((error) => {
+      console.error('Error adding document: ', error);
+    });
+};
